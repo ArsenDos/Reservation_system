864c8f67eace68a2e4727456d0d0d4bbf688f8e6
Arsen <dosarsen112@gmail.com>
2025-11-28 11:55:44 +0500

    change and validate

--- src/main/java/kz/com/reservation/reservation/task/entity/Task.java
+++ src/main/java/kz/com/reservation/reservation/task/entity/Task.java
@@ -3,18 +3,32 @@ package kz.com.reservation.reservation.task.entity;
 import jakarta.persistence.*;
 import kz.com.reservation.reservation.task.enums.TaskPriority;
 import kz.com.reservation.reservation.task.enums.TaskStatus;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="tasks")
 public class Task {
+
+
     @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(name="creator_id", nullable=false)
+    Long creatorId;
+    @Column(name="assigneed_id", nullable=false)
+    Long assignedId;
+    @Column(name="task_status")
+    @Enumerated(EnumType.STRING)
+    TaskStatus status;
+    @Column(name="created" , nullable=false)
+    LocalDateTime created;
+    @Column(name="deadline_date")
+    LocalDateTime deadlineDate;
+    @Column(name="priority")
+    @Enumerated(EnumType.STRING)
+    TaskPriority priority;
+
 
     public Long getId() {
         return id;
@@ -71,19 +85,4 @@ public class Task {
     public void setPriority(TaskPriority priority) {
         this.priority = priority;
     }
-
-    @Column(name="creator_id", nullable=false)
-    Long creatorId;
-    @Column(name="assigneed_id", nullable=false)
-    Long assignedId;
-    @Column(name="task_status")
-    @Enumerated(EnumType.STRING)
-    TaskStatus status;
-    @Column(name="created" , nullable=false)
-    LocalDateTime created;
-    @Column(name="deadline_date" , nullable=false)
-    LocalDateTime deadlineDate;
-    @Column(name="priority")
-    @Enumerated(EnumType.STRING)
-    TaskPriority priority;
 }
